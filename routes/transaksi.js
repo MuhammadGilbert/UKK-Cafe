76ebecc8dcfec64ce938849d6941f861ac8420b6
Gilbert <gilbertariel13@gmail.com>
2023-03-14 14:28:23 +0700

    Add new endpoint

--- routes/transaksi.js
+++ routes/transaksi.js
@@ -7,12 +7,14 @@ const {
   bayar,
   updateTransaksi,
   addTransaksi,
+  addDetail,
 } = require("../controllers/transaksi");
 
 router.get("/", displayAllTransaksi);
 router.get("/:id", displayTransaksi);
 router.post("/", addTransaksi, checkAuth);
-router.post("/:id", bayar, checkAuth);
+router.post("/detail/", addDetail, checkAuth);
+router.put("/bayar/:id", bayar, checkAuth);
 router.put("/:id_Transaksi", updateTransaksi, checkAuth);
 
 module.exports = router;
