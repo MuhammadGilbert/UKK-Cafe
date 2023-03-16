0d1953d60810bf4ccf16df4314362812aa95843b
Gilbert <gilbertariel13@gmail.com>
2023-03-16 13:42:38 +0700

    Update SQL

--- project_ukk.sql
+++ project_ukk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Mar 2023 pada 11.27
+-- Waktu pembuatan: 16 Mar 2023 pada 07.42
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -31,9 +31,18 @@ CREATE TABLE `detail_transaksi` (
   `id_detail_transaksi` int(11) NOT NULL,
   `id_transaksi` int(11) NOT NULL,
   `id_menu` int(11) NOT NULL,
-  `harga` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail_transaksi`, `id_transaksi`, `id_menu`, `jumlah`) VALUES
+(1, 1, 1, 2),
+(2, 1, 3, 2),
+(3, 2, 6, 3),
+(4, 3, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -54,9 +63,11 @@ CREATE TABLE `meja` (
 INSERT INTO `meja` (`id_meja`, `nomor_meja`, `status_meja`) VALUES
 (1, '1', 'available'),
 (2, '2', 'available'),
-(3, '3', 'available'),
-(4, '4', 'available'),
-(5, '5', 'available');
+(3, '3', 'unavailable'),
+(4, '4', 'unavailable'),
+(5, '5', 'available'),
+(6, '6', 'available'),
+(7, '7', 'available');
 
 -- --------------------------------------------------------
 
@@ -79,10 +90,11 @@ CREATE TABLE `menu` (
 
 INSERT INTO `menu` (`id_menu`, `nama_menu`, `jenis`, `deskripsi`, `gambar`, `harga`) VALUES
 (1, 'Nasi goreng', 'makanan', 'Nasi goreng dengan bumbu pilihan terbaik', 'img-1675826720654.jpg', 15000),
-(2, 'Bakso', 'makanan', 'Bakso dengan daging sapi yang berkualitas dan yang pastinya lembut', 'img-1675827423687.jfif', 12000),
+(2, 'Bakso', 'makanan', 'Bakso dengan daging sapi yang berkualitas dan yang pastinya lembut', 'img-1678941509551.jpg', 14000),
 (3, 'Es teh', 'minuman', 'Teh dengan es', 'img-1675827541526.jpg', 5000),
 (4, 'Cappucino', 'minuman', 'Cappucino enak dah cobain deh', 'img-1675827669480.jpg', 7500),
-(5, 'French Fries', 'makanan', 'French fries yang beneran enak serius gak boong', 'img-1675827809060.jpg', 10000);
+(5, 'French Fries', 'makanan', 'French fries yang beneran enak serius gak boong', 'img-1675827809060.jpg', 10000),
+(6, 'Croissant', 'makanan', 'Sejenis pastry atau kue kering yang berasal dari Prancis', 'img-1678940874513.jpeg', 20000);
 
 -- --------------------------------------------------------
 
@@ -98,6 +110,15 @@ CREATE TABLE `transaksi` (
   `nama_pelanggan` varchar(100) NOT NULL,
   `status` enum('belum_bayar','lunas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `tgl_transaksi`, `id_user`, `id_meja`, `nama_pelanggan`, `status`) VALUES
+(1, '2023-03-16 05:42:29', 2, 1, 'Krisna', 'lunas'),
+(2, '2023-03-16 05:16:45', 2, 3, 'Gilbert', 'belum_bayar'),
+(3, '2023-03-16 05:35:50', 3, 4, 'Gatot', 'belum_bayar');
 
 -- --------------------------------------------------------
 
@@ -118,7 +139,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_user`, `role`, `username`, `password`) VALUES
-(1, 'admin', 'admin', 'admin', '$2b$10$nmOoRhCA6v316XTUD105N.7Kwra9prRChUNK9WsuW1x1mXewsEflW');
+(1, 'admin', 'admin', 'admin', '$2b$10$nmOoRhCA6v316XTUD105N.7Kwra9prRChUNK9WsuW1x1mXewsEflW'),
+(2, 'Komarudin', 'kasir', 'udin', '$2b$10$rrUaQcPoqZRmYXqgQo6l6OgBf6YDu7uZ/wx.7wBXdrp6q1iPZ8Te.'),
+(3, 'Jamal Maulana', 'manajer', 'jamal', '$2b$10$jnJ3gtfSnsZLgd1o5XVzXeopVks4j.YNeMThRIGkSLCrp5m.GspuW');
 
 --
 -- Indexes for dumped tables
@@ -129,9 +152,8 @@ INSERT INTO `user` (`id_user`, `nama_user`, `role`, `username`, `password`) VALU
 --
 ALTER TABLE `detail_transaksi`
   ADD PRIMARY KEY (`id_detail_transaksi`),
-  ADD KEY `id_transaksi` (`id_transaksi`,`id_menu`,`harga`),
-  ADD KEY `id_menu` (`id_menu`),
-  ADD KEY `harga` (`harga`);
+  ADD KEY `id_transaksi` (`id_transaksi`,`id_menu`),
+  ADD KEY `id_menu` (`id_menu`);
 
 --
 -- Indeks untuk tabel `meja`
@@ -168,31 +190,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `meja`
 --
 ALTER TABLE `meja`
-  MODIFY `id_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -203,8 +225,7 @@ ALTER TABLE `user`
 --
 ALTER TABLE `detail_transaksi`
   ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_transaksi_ibfk_4` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_transaksi_ibfk_5` FOREIGN KEY (`harga`) REFERENCES `menu` (`harga`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `detail_transaksi_ibfk_4` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `transaksi`
